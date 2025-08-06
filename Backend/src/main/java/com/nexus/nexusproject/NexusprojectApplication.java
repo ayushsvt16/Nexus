c6f528aff53273c6e7771bb893b0c45a2c3621e3
Shivansh Tiwari <stshivansh354@gmail.com>
2025-08-06 17:47:58 +0530

    working login

--- Backend/src/main/java/com/nexus/nexusproject/NexusprojectApplication.java
+++ Backend/src/main/java/com/nexus/nexusproject/NexusprojectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NexusprojectApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(NexusprojectApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NexusprojectApplication.class, args);
+    }
 }
