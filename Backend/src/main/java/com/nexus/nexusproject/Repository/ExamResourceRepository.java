be89a4b99962d4944a9d304c962791f4630fd008
Shivansh Tiwari <stshivansh354@gmail.com>
2025-08-13 01:11:51 +0530

    Year-Filter Updated

--- Backend/src/main/java/com/nexus/nexusproject/Repository/ExamResourceRepository.java
+++ Backend/src/main/java/com/nexus/nexusproject/Repository/ExamResourceRepository.java
@@ -10,4 +10,9 @@ import com.nexus.nexusproject.model.ExamResource;
 @Repository
 public interface ExamResourceRepository extends JpaRepository<ExamResource, Long> {
     List<ExamResource> findBySemesterAndBranchAndType(Integer semester, String branch, String type);
+    List<ExamResource> findBySemesterAndBranchAndTypeAndYear(Integer semester, String branch, String type, Integer year);
+    List<ExamResource> findByYear(Integer year);
+    List<ExamResource> findBySemester(Integer semester);
+    List<ExamResource> findByBranch(String branch);
+    List<ExamResource> findByType(String type);
 }
